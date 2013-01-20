68c23a42aa19aa14679996e721af019b8799c5b9
Ian Crosby <ian.crosby@10sheet.com>
2013-01-20 16:27:13 -0500

    First attempt at getting JS running

--- app.py
+++ app.py
@@ -2,19 +2,10 @@ import os
 from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
-import pyrise
-
-pyrise.Highrise.set_server('https://testingaccount1.highrisehq.com/')
-pyrise.Highrise.auth('8d067f661c6611c3c3e40b245dd9de37')
 
 @app.route('/')
 def index():
-    p = pyrise.Person()
-    p.first_name = 'My_first_test'
-    p.last_name = "Yippee"
-    p.contact_data.email_addresses.append(pyrise.EmailAddress(address="joe@schmoe.com"))
-    p.save()
-    return null
+    return render_template('index.html')
 
 
 
---
+++ templates/addToHighrise.js
@@ -0,0 +1,12 @@
+alert("imported")
+
+function addToHighrise()) {
+
+$.getJSON('SET_URL_HERE', {
+	name: "test_name"
+	}, function() {
+		alert("success")
+	});
+};
+	
+// first_name, last_name, e_mail, company, country
