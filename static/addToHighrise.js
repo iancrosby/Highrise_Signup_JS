75e8dd7066302c9dfffc2211393741a141b75b22
Ian Crosby <ian.crosby@10sheet.com>
2013-01-21 14:50:41 -0500

    Revert "Debugging"
    This reverts commit 6c962c06fa59bb8dc546ee8182dbf91b4b64a41f.

--- static/addToHighrise.js
+++ static/addToHighrise.js
@@ -1,12 +1,11 @@
 
 
-function addToHighrise(first_name, last_name, e_mail, company, country) {
+function addToHighrise(cname, email, company, country) {
 
 // $.getJSON('http://sheltered-island-3519.herokuapp.com' + '/add_to_highrise', {
 $.getJSON($SCRIPT_ROOT + '/add_to_highrise', {
-	first_name: first_name,
-	last_name: last_name,
-	e_mail: e_mail,
+	cname: cname,
+	email: email,
 	company: company,
 	country: country
 	}, function() {
@@ -14,5 +13,5 @@ $.getJSON($SCRIPT_ROOT + '/add_to_highrise', {
 
 };
 	
-addToHighrise("Ian", "Crosby", "a@a.a", "USCo", "USofA")
+addToHighrise("Ian Crosby", "a@a.cc", "USCo", "USofA")
 
