4850d91d458ddbd7b273c66bcef7397c7685b972
Ian Crosby <ian.crosby@10sheet.com>
2013-01-21 14:06:04 -0500

    Ask for single name, not working

--- app.py
+++ app.py
@@ -1,19 +1,63 @@
 import os
 from flask import Flask, jsonify, render_template, request
+from datetime import datetime, timedelta
 app = Flask(__name__)
 
+import pyrise
 
+# Log into the Highrise server
+pyrise.Highrise.set_server('https://testingaccount1.highrisehq.com/')
+pyrise.Highrise.auth('8d067f661c6611c3c3e40b245dd9de37')
+ 
+# This is the list of tasks that should be attached to each person
+task_template = []
+task_template.append(['Send welcome email',1])
+task_template.append(['Schedule phone call',7])
+task_template.append(['First phone call',14])
+task_template.append(['Convert to paid or end trial',40])
+task_template.append(['Change tag to reflect new status',40])
+
+
+# This is just for testing purposes, to make sure the app is running properly
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
+
 
 @app.route('/add_to_highrise')
 def add_hr():
-	first_name = request.args.get('first_name')
-	last_name = request.args.get('last_name')
-	e_mail = request.args.get('e_mail')
+	'''Receives an signup request from JSON, sends it to Highrise with attached signup tasks'''
+
+	#Receive JSON data
+	cname = request.args.get('name')
+	email = request.args.get('email')
 	company = request.args.get('company')
 	country = request.args.get('country')
+	
+	first_name = cname[:cname.find(" ")]
+	last_name = cname[cname.find(" ")+1:]
+	
+	#Create new person in Highrise
+	p = pyrise.Person()
+	p.first_name = first_name
+	p.last_name = last_name		
+	p.contact_data.email_addresses.append(pyrise.EmailAddress(address=email))
+	
+	p.save()
+	
+	p.add_note('Company name: ' + company + ", located in " + country)
+	p.add_tag('Website signup')
+	
+	# Add tasks to the person
+	for eachtask in task_template:
+		
+		t = pyrise.Task()
+		t.body = eachtask[0]
+		t.due_at = datetime.now() + timedelta(days=eachtask[1])
+		t.subject_id = p.id	
+		t.subject_type = 'Party'	
+		t.save()
+	
 	return jsonify(result=first_name + " " + last_name + " " + country)
 
 
--- static/addToHighrise.js
+++ static/addToHighrise.js
@@ -1,17 +1,17 @@
-function addToHighrise(first_name, last_name, e_mail, company, country) {
 
-$.getJSON('http://sheltered-island-3519.herokuapp.com' + '/add_to_highrise', {
-	first_name: first_name,
-	last_name: last_name,
-	e_mail: e_mail,
+
+function addToHighrise(name, email, company, country) {
+
+// $.getJSON('http://sheltered-island-3519.herokuapp.com' + '/add_to_highrise', {
+$.getJSON($SCRIPT_ROOT + '/add_to_highrise', {
+	name: name,
+	email: email,
 	company: company,
 	country: country
-	}, function(x) {
-		alert(x.result)
+	}, function() {
 	});
 
 };
 	
-addToHighrise("Ian", "Crosby", "a@a.a", "USCo", "USofA")
+addToHighrise("Ian Crosby", "a@a.cc", "USCo", "USofA")
 
-alert("script done")
